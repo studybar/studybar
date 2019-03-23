1cb33ca5da34215c7e39b462e3d0430818412b1d
Maary <dicedsteve@gmail.com>
2019-03-24 00:36:13 +0800

    在 UserFragment 中添加了头像显示（只有占位符作用，具体布局后续修改）及设置按钮（外观待修改），增加跳转到 SettingsActivity （具体内容尚未设置）。

---
+++ app/src/main/java/com/wedo/studybar/SettingsActivity.java
@@ -0,0 +1,6 @@
+package com.wedo.studybar;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/wedo/studybar/UserFragment.java
+++ app/src/main/java/com/wedo/studybar/UserFragment.java
@@ -1,5 +1,6 @@
 package com.wedo.studybar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class UserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_user,container,false);
+
+        Button settingsButton = (Button) rootView.findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent settingsIntent = new Intent(getActivity(),SettingsActivity.class);
+
+                startActivity(settingsIntent);
+            }
+        });
+
+
         return rootView;
     }
 
