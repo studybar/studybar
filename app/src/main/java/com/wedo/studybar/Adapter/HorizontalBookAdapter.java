619defd50e1e3791bdc160d161c8e2cd1d9fe1a5
Maary <dicedsteve@gmail.com>
2019-04-04 01:14:50 +0800

    1. 加入首页显示横排书籍相应 layout、HorizontalBookAdapter 及 HomeFragment 中代码 2. 优化部分 HomeFragment 中代码

---
+++ app/src/main/java/com/wedo/studybar/Adapter/HorizontalBookAdapter.java
@@ -0,0 +1,84 @@
+package com.wedo.studybar.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wedo.studybar.R;
+
+import java.util.List;
+
+public class HorizontalBookAdapter extends RecyclerView.Adapter<HorizontalBookAdapter.horizontalBookViewHolder> {
+
+    private List<String> mHorizontalBookNames;
+    private List<String> mHorizontalBookAuthors;
+    private List<Integer> mHorizontalBookCovers;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    public HorizontalBookAdapter(Context context,List<Integer> horizontalBookCovers, List<String> horizontalBookNames, List<String> horizontalBookAuthors){
+        this.mInflater = LayoutInflater.from(context);
+        this.mHorizontalBookCovers = horizontalBookCovers;
+        this.mHorizontalBookNames = horizontalBookNames;
+        this.mHorizontalBookAuthors = horizontalBookAuthors;
+    }
+
+    @NonNull
+    @Override
+    public horizontalBookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.horizontal_list_book,parent,false);
+        return  new horizontalBookViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HorizontalBookAdapter.horizontalBookViewHolder horizontalBookViewHolder, int position) {
+        int horizontalBookCover = mHorizontalBookCovers.get(position);
+        String horizontalBookName = mHorizontalBookNames.get(position);
+        String horizontalBookAuthor = mHorizontalBookAuthors.get(position);
+        horizontalBookViewHolder.horizontalBookCoverView.setImageResource(horizontalBookCover);
+        horizontalBookViewHolder.horizontalBookNameView.setText(horizontalBookName);
+        horizontalBookViewHolder.horizontalBookAuthorView.setText(horizontalBookAuthor);
+    }
+
+    public class horizontalBookViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+        ImageView horizontalBookCoverView;
+        TextView horizontalBookNameView;
+        TextView horizontalBookAuthorView;
+
+        horizontalBookViewHolder(View itemView){
+            super(itemView);
+            horizontalBookCoverView = itemView.findViewById(R.id.book_cover);
+            horizontalBookNameView = itemView.findViewById(R.id.book_name);
+            horizontalBookAuthorView = itemView.findViewById(R.id.book_author);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(mClickListener != null)
+                mClickListener.onItemClick(view,getAdapterPosition());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHorizontalBookNames.size();
+    }
+
+    public String getItem(int id){
+        return mHorizontalBookNames.get(id);
+    }
+
+    public void setClickListener(ItemClickListener itemClickListener){
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
--- app/src/main/java/com/wedo/studybar/Fragments/HomeFragment.java
+++ app/src/main/java/com/wedo/studybar/Fragments/HomeFragment.java
@@ -13,19 +13,24 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.wedo.studybar.Adapter.CategoryAdapter;
+import com.wedo.studybar.Adapter.HorizontalBookAdapter;
 import com.wedo.studybar.R;
 
 import java.util.ArrayList;
 
-public class HomeFragment extends Fragment implements CategoryAdapter.ItemClickListener{
+public class HomeFragment extends Fragment {
 
     private CategoryAdapter mCategoryAdapter;
+    private HorizontalBookAdapter mHorizontalBookAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home,container,false);
 
+        /**
+         * to show category
+         * */
         //data to populate the category RecyclerView with
         ArrayList<String> categoryNames = new ArrayList<>();
         categoryNames.add("Philosophy");
@@ -46,39 +51,61 @@ public class HomeFragment extends Fragment implements CategoryAdapter.ItemClickL
         LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
         categoryRecyclerView.setLayoutManager(horizontalLayoutManager);
         mCategoryAdapter = new CategoryAdapter(getActivity(),categoryNames);
-        mCategoryAdapter.setClickListener(this);
+        mCategoryAdapter.setClickListener(new CategoryAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getActivity(),mCategoryAdapter.getItem(position),Toast.LENGTH_SHORT).show();
+            }
+        });
         categoryRecyclerView.setAdapter(mCategoryAdapter);
+
+        /**
+         * to show books
+         * */
+        ArrayList<Integer> horizontalBookCovers = new ArrayList<>();
+        horizontalBookCovers.add(R.drawable.test);
+        horizontalBookCovers.add(R.drawable.test);
+        horizontalBookCovers.add(R.drawable.test);
+        horizontalBookCovers.add(R.drawable.test);
+        horizontalBookCovers.add(R.drawable.test);
+        horizontalBookCovers.add(R.drawable.test);
+
+        ArrayList<String> horizontalBookNames = new ArrayList<>();
+        horizontalBookNames.add("习近平谈治国理政");
+        horizontalBookNames.add("习近平谈治国理政");
+        horizontalBookNames.add("习近平谈治国理政");
+        horizontalBookNames.add("习近平谈治国理政");
+        horizontalBookNames.add("习近平谈治国理政");
+        horizontalBookNames.add("习近平谈治国理政");
+
+        ArrayList<String> horizontalBookAuthors = new ArrayList<>();
+        horizontalBookAuthors.add("习近平");
+        horizontalBookAuthors.add("习近平");
+        horizontalBookAuthors.add("习近平");
+        horizontalBookAuthors.add("习近平");
+        horizontalBookAuthors.add("习近平");
+        horizontalBookAuthors.add("习近平");
+
+        RecyclerView horizontalBooksRecyclerView = rootView.findViewById(R.id.top_books_recycler_view);
+        LinearLayoutManager horizontalLayoutManager_forTop = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+        horizontalBooksRecyclerView.setLayoutManager(horizontalLayoutManager_forTop);
+        mHorizontalBookAdapter = new HorizontalBookAdapter(getActivity(),horizontalBookCovers,horizontalBookNames,horizontalBookAuthors);
+        mHorizontalBookAdapter.setClickListener(new HorizontalBookAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(getActivity(),mHorizontalBookAdapter.getItem(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+        horizontalBooksRecyclerView.setAdapter(mHorizontalBookAdapter);
+
+
+
         return rootView;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*
-        //data to populate the category RecyclerView with
-        ArrayList<String> categoryNames = new ArrayList<>();
-        categoryNames.add("Philosophy");
-        categoryNames.add("Economics");
-        categoryNames.add("Jurisprudence");
-        categoryNames.add("Pedagogy");
-        categoryNames.add("Literature");
-        categoryNames.add("History");
-        categoryNames.add("Formal science");
-        categoryNames.add("Engineering");
-        categoryNames.add("Agronomy");
-        categoryNames.add("Medicine");
-        categoryNames.add("Military science");
-        categoryNames.add("Management");
-        categoryNames.add("Art Theory");
-
-        RecyclerView categoryRecyclerView = (RecyclerView) getActivity().findViewById(R.id.categories_recycler_view);
-        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
-        categoryRecyclerView.setLayoutManager(horizontalLayoutManager);
-        mCategoryAdapter = new CategoryAdapter(getActivity(),categoryNames);
-        mCategoryAdapter.setClickListener(this);
-        categoryRecyclerView.setAdapter(mCategoryAdapter);
-        */
 
     }
 
@@ -88,8 +115,4 @@ public class HomeFragment extends Fragment implements CategoryAdapter.ItemClickL
 
     }
 
-    @Override
-    public void onItemClick(View view, int position) {
-        Toast.makeText(getActivity(),mCategoryAdapter.getItem(position),Toast.LENGTH_SHORT).show();
-    }
 }
