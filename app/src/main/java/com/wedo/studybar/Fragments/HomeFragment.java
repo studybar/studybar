dca5d53aee768d55c82c95883806bb7d95b4d9d5
Maary <dicedsteve@gmail.com>
2019-04-11 15:52:50 +0800

    验证：通过 Java code 在主页添加 TextView 和 RecyclerView 项目 布局微调

--- app/src/main/java/com/wedo/studybar/Fragments/HomeFragment.java
+++ app/src/main/java/com/wedo/studybar/Fragments/HomeFragment.java
@@ -1,15 +1,21 @@
 package com.wedo.studybar.Fragments;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.wedo.studybar.Adapter.CategoryAdapter;
 import com.wedo.studybar.Adapter.HorizontalBookAdapter;
@@ -33,20 +39,20 @@ public class HomeFragment extends Fragment {
         /**
          * to show category
          * */
-        ArrayList<String> categroyIds = new ArrayList<>();
-        categroyIds.add("2203467901");
-        categroyIds.add("2203467902");
-        categroyIds.add("2203467903");
-        categroyIds.add("2203467904");
-        categroyIds.add("2203467905");
-        categroyIds.add("2203467906");
-        categroyIds.add("2203467907");
-        categroyIds.add("2203467908");
-        categroyIds.add("2203467909");
-        categroyIds.add("2203467910");
-        categroyIds.add("2203467911");
-        categroyIds.add("2203467912");
-        categroyIds.add("2203467913");
+        ArrayList<String> categoryIds = new ArrayList<>();
+        categoryIds.add("2203467901");
+        categoryIds.add("2203467902");
+        categoryIds.add("2203467903");
+        categoryIds.add("2203467904");
+        categoryIds.add("2203467905");
+        categoryIds.add("2203467906");
+        categoryIds.add("2203467907");
+        categoryIds.add("2203467908");
+        categoryIds.add("2203467909");
+        categoryIds.add("2203467910");
+        categoryIds.add("2203467911");
+        categoryIds.add("2203467912");
+        categoryIds.add("2203467913");
 
         //data to populate the category RecyclerView with
         ArrayList<Integer> categoryNames = new ArrayList<>();
@@ -67,7 +73,7 @@ public class HomeFragment extends Fragment {
         RecyclerView categoryRecyclerView = (RecyclerView) rootView.findViewById(R.id.categories_recycler_view);
         LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
         categoryRecyclerView.setLayoutManager(horizontalLayoutManager);
-        mCategoryAdapter = new CategoryAdapter(getActivity(),categroyIds,categoryNames);
+        mCategoryAdapter = new CategoryAdapter(getActivity(),categoryIds,categoryNames);
         mCategoryAdapter.setClickListener(new CategoryAdapter.ItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
@@ -146,6 +152,48 @@ public class HomeFragment extends Fragment {
         });
         bookRecommendationOne.setAdapter(mHorizontalBookAdapter_for_one);
 
+        Resources resources = getContext().getResources();
+        int sixteenDp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                16,
+                resources.getDisplayMetrics()
+        );
+        int eightDp = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                8,
+                resources.getDisplayMetrics()
+        );
+
+        LinearLayout.LayoutParams paramsTextView = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        paramsTextView.setMargins(sixteenDp,sixteenDp,sixteenDp,sixteenDp);
+
+        LinearLayout.LayoutParams paramsRecyclerView = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        paramsRecyclerView.setMargins(sixteenDp,0,0,0);
+
+        LinearLayout homeLayout = (LinearLayout)rootView.findViewById(R.id.home_layout);
+
+        TextView bookRecommendationEconomics = new TextView(getActivity());
+            bookRecommendationEconomics.setText(R.string.Economics);
+            bookRecommendationEconomics.setTextAppearance(R.style.TextAppearance_AppCompat_Large);
+            bookRecommendationEconomics.setTypeface(Typeface.DEFAULT_BOLD);
+            bookRecommendationEconomics.setPadding(eightDp,0,0,0);
+            bookRecommendationEconomics.setLayoutParams(paramsTextView);
+
+        RecyclerView bookRecommendationEconomics_RecyclerView = new RecyclerView(getActivity());
+            LinearLayoutManager horizontalLayoutManager_forEconomics = new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false);
+            bookRecommendationEconomics_RecyclerView.setLayoutManager(horizontalLayoutManager_forEconomics);
+            bookRecommendationEconomics_RecyclerView.setAdapter(mHorizontalBookAdapter);
+            bookRecommendationEconomics_RecyclerView.setLayoutParams(paramsRecyclerView);
+
+
+        homeLayout.addView(bookRecommendationEconomics);
+        homeLayout.addView(bookRecommendationEconomics_RecyclerView);
 
         return rootView;
     }
